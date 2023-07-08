34072d3857e84553babd981a8814fe497ebb8e2a
Zakaria moulayrchid <zakmoulay07@gmail.com>
2023-07-08 12:33:48 -0400

    The basic solar sysytem is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.xml.transform.SourceLocator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystemBasic Omnax =new SolarSystemBasic();
+        Omnax.Moon="2";
+        Omnax.Sun="1";
+        Omnax.Planets="2";
+        Omnax.Star="4";
+
+
+
+
+
+    }
+}
